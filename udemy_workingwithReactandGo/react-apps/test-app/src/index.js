df2cd866af77b6abe5c91a1ec1c1257397e6490f
hackerrank <ea_ehd021279IT@yahoo.com>
2022-05-10 00:11:13 +0800

    updates

--- udemy_workingwithReactandGo/react-apps/test-app/src/index.js
+++ udemy_workingwithReactandGo/react-apps/test-app/src/index.js
@@ -5,9 +5,9 @@ import AppFooter from './AppFooter';
 class App extends Component {
 
     //this is required for all react component
-    render(){
+    render(){      
       return (
-        <div>
+        <div className='app'>
           <h1>Hello, world!</h1>
         <div>
         </div>
