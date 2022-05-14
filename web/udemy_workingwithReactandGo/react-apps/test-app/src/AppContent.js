363456dc405f827a532c8cca117ffd7c164ea81e
hackerrank <ea_ehd021279IT@yahoo.com>
2022-05-14 23:25:25 +0800

    Updated

--- web/udemy_workingwithReactandGo/react-apps/test-app/src/AppContent.js
+++ web/udemy_workingwithReactandGo/react-apps/test-app/src/AppContent.js
@@ -5,7 +5,7 @@ export default class AppContent extends Component {
     render(){
         return (
             <p>
-                This is my content.
+                This is my content!
                 <br/>
                 <button class="btn btn-primary" href="#"> My Button</button>
             </p>
